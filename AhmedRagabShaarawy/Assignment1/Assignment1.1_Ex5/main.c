fab0eee58c4471e8e7db9e4060e61fa4c3d701e8
AhmedRgShwy <ahmedragab.shaarawy@gmail.com>
2016-07-24 08:14:52 -0500

    Hello embedded world

---
+++ AhmedRagabShaarawy/Assignment1/Assignment1.1_Ex5/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char x;
+
+    printf("Enter a character: ");
+    scanf("%c", &x);
+
+    printf("ASCII value of %c = %d", x, x);
+
+
+    return 0;
+}
