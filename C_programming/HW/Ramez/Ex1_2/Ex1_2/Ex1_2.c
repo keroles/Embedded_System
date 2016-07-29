ea811a523e6fe2f4680f8ea40f69fd41928aa8a1
Ramez Shendy <Ramez Shendy>
2016-07-30 01:34:24 +0200

    homework 1, 2 and 3

---
+++ C_programming/HW/Ramez/Ex1_2/Ex1_2/Ex1_2.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+
+void main(){
+
+	int x;
+
+	printf("enter integer \n");
+	scanf("%d",&x);
+	printf("you entered %d \n", x);
+
+	system("pause");
+}
---
+++ C_programming/HW/Ramez/Ex1_4/Ex1_4/Ex1_4.c
@@ -0,0 +1,10 @@
+#include "stdio.h"
+
+void main(){
+	float x, y;
+
+	printf("enter two number \n");
+	scanf("%f %f", &x, &y);
+	printf("the product is %f \n", x*y);
+	system("pause");
+}
