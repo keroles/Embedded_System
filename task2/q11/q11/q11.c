7551bbc9fc2503d7e3dc367cb32f5dfca851c18c
Keroles <eng.keroles.karam@gmail.com>
2016-09-23 08:22:26 +0200

    organize git

--- task2/q11/q11/q11.c
+++
@@ -1,6 +0,0 @@
-#include"stdio.h"
-int main()
-{
-	printf("C Programming\n");
-	system("pause");
-}
--- task2/q13/q13/q13.c
+++
@@ -1,9 +0,0 @@
-#include"stdio.h"
-int main()
-{
-	char character;
-	printf(" character : ");
-	scanf("%c",&character);
-	printf("ascii of the character  = %d\n",character);
-	system("pause");
-}
